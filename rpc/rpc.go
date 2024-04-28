76adbb6fd9be55ca04f8db89db2e2a245811a011
exzork <muhammadeko.if@gmail.com>
2024-04-28 17:30:17 +0700

    log

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -35,7 +35,7 @@ func RegisterRPCFunction(name string, f func(params ...interface{}) (interface{}
 }
 
 func CallRPC(request RPCRequest, dst interface{}) error {
-	ch, err := message.Conn.Channel()
+	ch, err := message.GetChannel()
 	q, err := ch.QueueDeclare("", false, false, true, false, nil)
 	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
 	corrID, err := message.GenerateRandomString(32)
