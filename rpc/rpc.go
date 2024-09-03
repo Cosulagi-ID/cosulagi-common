c3edc827319e751e17347b8571ec66ef60c13f85
exzork <muhammadeko.if@gmail.com>
2024-09-03 10:33:32 +0700

    test fix

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -70,12 +70,15 @@ func CallRPC(name string, dst interface{}, params ...interface{}) error {
 	for d := range message.Msgs {
 		if d.ContentType == "text/plain" {
 			err = errors.New(string(d.Body))
+			_ = d.Reject(false)
 			break
 		}
 		_ = json.Unmarshal(d.Body, dst)
+		err = d.Ack(false)
 		break
 	}
-	return nil
+
+	return err
 }
 
 func RPCServer() {
